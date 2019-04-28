317eb1c11b281ce598f0e7cb6226267cbf287826
jliel <kiritobaz@gmail.com>
2019-04-28 14:36:32 -0500

    cambio a postgresql

--- restful01/settings.py
+++ restful01/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'restful01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drones',
+        'USER': 'django_admin',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
